335bd9869d826833613517260acdc83c0006f6d1
Victor Ruch <viruch3112@gmail.com>
2017-09-26 16:58:19 +0200

    removing dataset from start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #docker run --rm -d -v $PWD/tf-model:/tf-model -p 9000:9000 facenet-serving 
-docker run --rm -it -v $PWD/facenet-model:/base-model -v $PWD/src:/facenet -v ~/our_dataset:/face-data/ -p 5000:5000 facenet-rest-server
+docker run --rm -it -v $PWD/facenet-model:/base-model -v $PWD/src:/facenet -p 5000:5000 facenet-rest-server
