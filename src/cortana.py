18febce43cc6c7501e79a68c95b72a33f085f91d
Victor Ruch <viruch3112@gmail.com>
2017-10-13 15:07:30 +0200

    adding working verifier

--- src/cortana.py
+++ src/cortana.py
@@ -5,11 +5,12 @@ import time
 
 import math
 from cognitive_face import CognitiveFaceException
+from itertools import islice
 
 FACE_PATH = '/home/viruch/Documents/projects/SA/PersonalFaceData/'
 trainFaces = 7
 testFaces = 3
-
+PERSON_SIZE = 10
 
 class FaceAPI:
     CALL_LIMIT_PER_MINUTE = 20
@@ -24,18 +25,32 @@ class FaceAPI:
     def __init__(self, group_id):
         self.group_id = group_id
 
+    def __cache(self, file_path, call):
+        cache_file = file_path + ".json"
+        if os.path.isfile(cache_file):
+            with open(cache_file, 'r') as f:
+                result = json.load(f)
+        else:
+            result = call()
+            with open(cache_file, 'w') as f:
+                json.dump(result, f)
+        return result
+
     def wait(self, return_value):
         time.sleep(4)
         return return_value
 
     def detect_face(self, image_path):
-        return self.wait(CF.face.detect(image_path))
+        return self.__cache(image_path,
+                            lambda: self.wait(CF.face.detect(image_path)))
 
     def create_person_group(self):
         return self.wait(CF.person_group.create(self.group_id))
 
-    def create_person(self, dirname):
-        return self.wait(CF.person.create(self.group_id, dirname))
+    def create_person(self, root, dirname):
+        return self.__cache(os.path.join(root, dirname),
+                            lambda: self.wait(CF.person.create(self.group_id, dirname)))
+
 
     def train(self):
         CF.person_group.train(self.group_id)
@@ -53,7 +68,8 @@ class FaceAPI:
             pass
 
     def addFace(self, imagePath, personId, target_face):
-        return self.wait(CF.person.add_face(imagePath, self.group_id, personId, target_face=target_face))
+        return self.__cache(imagePath,
+                         lambda: self.wait(CF.person.add_face(imagePath, self.group_id, personId, target_face=target_face)))
 
     def list(self):
         return self.wait(CF.person.lists(self.group_id))
@@ -70,11 +86,10 @@ class FaceAPI:
 
 faceAPI = FaceAPI('my_group')
 
-
 def getFiles(dirname):
-    files = [FACE_PATH + dirname + "/" + f for f in os.listdir(FACE_PATH + dirname) if not f.endswith(".json")]
-    files.sort()
-    return files
+    return sorted([FACE_PATH + dirname + "/" + f
+                   for f in os.listdir(FACE_PATH + dirname)
+                   if not f.endswith(".json")])
 
 
 def detectFace(absoluteName):
@@ -88,39 +103,35 @@ def detectFace(absoluteName):
     return ",".join(map(str, [face["left"], face["top"], face["width"], face["height"]]))
 
 
-def upload():
-    try:
-        faceAPI.create_person_group()
-    except CognitiveFaceException:
-        pass
-
-    for dirname in os.listdir(FACE_PATH):
+def get_all_faces():
+    for dirname in sorted(os.listdir(FACE_PATH)):
         if dirname.endswith(".json"):
             continue
         faces = getFiles(dirname)
         if len(faces) < trainFaces + testFaces:
             print dirname + " has not enough samples: " + str(len(faces))
             continue
-        if not os.path.isfile(FACE_PATH + dirname + ".json"):
-            result = faceAPI.create_person(dirname)
-            with open(FACE_PATH + dirname + '.json', 'w') as outfile:
-                json.dump(result, outfile)
-        else:
-            with open(FACE_PATH + dirname + '.json', 'r') as outfile:
-                result = json.load(outfile)
+        yield dirname, faces
+
+def getPersonFaces():
+    return islice(get_all_faces(), PERSON_SIZE)
+
+def upload():
+    try:
+        faceAPI.create_person_group()
+    except CognitiveFaceException:
+        pass
+
+    for dirname, faces in getPersonFaces():
+        result = faceAPI.create_person(FACE_PATH, dirname)
         print(result)
         personId = result["personId"]
         trains = faces[:trainFaces]
         for absoluteName in trains:
-            if os.path.isfile(absoluteName + ".json"):
-                continue
             target_face = detectFace(absoluteName)
             if target_face is None:
                 continue
-            result = faceAPI.addFace(absoluteName, personId, target_face)
-            print(result)
-            with open(absoluteName + '.json', 'w') as outfile:
-                json.dump(result, outfile)
+            print(faceAPI.addFace(absoluteName, personId, target_face))
             time.sleep(4)
 
 
@@ -129,55 +140,49 @@ def showServerContent():
     for p in people:
         print p["name"] + ": " + str(len(p["persistedFaceIds"])) + ":" + p["personId"]
 
-
 def verify():
-    testFaceIds = []
-    correctFaceToPersonIds = {}
+    verify_faces = {}
 
-    for dirname in os.listdir(FACE_PATH):
-        if dirname.endswith(".json"):
-            continue
-        faces = getFiles(dirname)
-        if len(faces) < trainFaces + testFaces:
-            print dirname + " has not enough samples: " + str(len(faces))
-            continue
+    for dirname, faces in getPersonFaces():
         tests = faces[trainFaces: trainFaces + testFaces]
         for absoluteName in tests:
-            if not os.path.isfile(absoluteName + ".json"):
-                result = faceAPI.detect_face(absoluteName)
-                with open(absoluteName + '.json', 'w') as outfile:
-                    json.dump(result, outfile)
-                time.sleep(4)
-            else:
-                with open(absoluteName + '.json', 'r') as outfile:
-                    result = json.load(outfile)
-            print (result)
+            result = faceAPI.detect_face(absoluteName)
             if len(result) == 0:
                 print "no face found: " + absoluteName
                 continue
-            currentFace = {}
-            currentFace["fileName"] = absoluteName
-            currentFace["person"] = dirname
-            face_id = result[0]["faceId"]
-            correctFaceToPersonIds[face_id] = currentFace
-            testFaceIds.append(face_id)
+            currentFace = {
+                "fileName": absoluteName,
+                "person":  dirname,
+            }
             with open(FACE_PATH + dirname + '.json', 'r') as outfile:
                 currentFace["personId"] = json.load(outfile)["personId"]
 
-    print correctFaceToPersonIds
+            verify_faces[result[0]["faceId"]] = currentFace
 
-    results = faceAPI.identify(testFaceIds)
+    results = faceAPI.identify(verify_faces.keys())
+
+    correct = 0
+    false = 0
 
     for r in results:
-        face_id = r["faceId"]
+        face = verify_faces[r["faceId"]]
         predicted = r["candidates"]
         if len(predicted) == 0:
-            print("no prediction: " + correctFaceToPersonIds[face_id])
+            print("no prediction: " + face["fileName"])
+            false += 1
+            continue
+        if face["personId"] == predicted[0]["personId"]:
+            correct +=1
+        else:
+            false += 1
+            print("missmatch for: " + face + " predicted: " + predicted)
+    print ("successrate: " + str(float(correct) / float(correct + false)))
 
 
-faceAPI.reset()
-upload()
-faceAPI.train()
-# verify()
 
-showServerContent()
+#faceAPI.reset()
+#upload()
+#faceAPI.train()
+verify()
+
+#showServerContent()
